80a4b0e71b27213c99e624b34acb2a0768181a4d
Reschivon <xchenbox@gmail.com>
2022-04-03 13:45:46 -0400

    import gazebo simulator

---
+++ src/gazebo_sim/DtSamples/drive/src/drive.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import rospy
+from std_msgs.msg import String
+from ackermann_msgs.msg import AckermannDriveStamped
+
+
+if __name__ == '__main__':
+	rospy.init_node('racecar_driver',anonymous=True)
+	pub = rospy.Publisher('/vesc/high_level/ackermann_cmd_mux/input/nav_0', AckermannDriveStamped,queue_size=10)
+
+	rate = rospy.Rate(20)
+	while not rospy.is_shutdown():
+		msg = AckermannDriveStamped()
+       		msg.drive.speed = 0.5;
+       		msg.drive.acceleration = 1;
+       		msg.drive.jerk = 1;
+       		msg.drive.steering_angle = 0.5;
+       		msg.drive.steering_angle_velocity = 1;
+
+		pub.publish(msg)
+	rospy.spin()
