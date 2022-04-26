06cc544a9c3661c67cb9802b241b14b77e9be4c8
William Dobert <wdobert@purdue.edu>
2022-04-26 19:33:56 -0400

    Modify loop traversal to handle clumped cones better

--- src/cone_finder/nodes/cone_finder_fake.py
+++ src/cone_finder/nodes/cone_finder_fake.py
@@ -13,19 +13,23 @@ def fake_loop():
 
 
 if __name__ == "__main__":
+    inner_radius = 1;
+    inner_count = 8;
+    outer_multiplier = 3;
+
     fake_cones = ConeList()
     fake_cones.x = []
     fake_cones.y = []
-    for i in range(48):
-        rad = (i / 48) * math.pi * 2
+    for i in range(int(inner_count * outer_multiplier)):
+        rad = (i / (inner_count * outer_multiplier)) * math.pi * 2 - (i % 3) * 0.15
         # outer ring
-        fake_cones.x.append(math.cos(rad) * 3)
-        fake_cones.y.append(math.sin(rad) * 3)
+        fake_cones.x.append(math.cos(rad) * inner_radius * outer_multiplier)
+        fake_cones.y.append(math.sin(rad) * inner_radius * outer_multiplier)
 
         # inner ring, less dense
-        if i % 3 == 0:
-            fake_cones.x.append(math.cos(rad))
-            fake_cones.y.append(math.sin(rad))
+        if i % outer_multiplier == 0:
+            fake_cones.x.append(math.cos(rad) * inner_radius)
+            fake_cones.y.append(math.sin(rad) * inner_radius)
 
     try:
         rospy.init_node("cone_finder")
